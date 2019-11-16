5f9088d71e2d03aec9df26170cd76bcc1cb8836c
Samuel Leeuwenburg <samleeuwenburg@gmail.com>
2019-11-16 12:27:14 +0100

    add database

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,16 @@
 import express = require('express');
+import sqlite3 = require('sqlite3');
 const app: express.Application = express();
+
+const db = new sqlite3.Database('./jukebox.db', sqlite3.OPEN_READWRITE, err => {
+    err ? console.error(err.message)
+        : console.log('Connected to jukebox.db database');
+});
 
 app.get('/', function (req, res) {
     res.send('Hello World!!');
 });
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Jukebox is playing on port 3000!');
 });
