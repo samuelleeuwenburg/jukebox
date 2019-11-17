be328657aa4ea0701418dab947563628bd68ca9e
Roy de Jager <roydejager@uitgeverij-deviant.nl>
2019-11-17 19:18:27 +0100

    Emit trackprogressupdate

--- server/app.ts
+++ server/app.ts
@@ -118,6 +118,7 @@ async function play() {
         if (!now.track) { return }
         if (now.cursor < now.track.duration_ms) {
             now.cursor += 100;
+            io.emit('trackProgressUpdate', now)
             return;
         }
 
