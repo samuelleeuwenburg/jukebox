9b26a19b97d88f7d28104768aa7eb53cd7bd7248
Samuel Leeuwenburg <samleeuwenburg@gmail.com>
2019-11-17 12:12:52 +0100

    wrap db calls into promises for async/await

--- server/app.ts
+++ server/app.ts
@@ -16,64 +16,63 @@ const app: express.Application = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/api/queue', (req, res) => {
+app.get('/api/queue', async (req, res) => {
     const db = getDb();
 
-    getQueue(db, (err, tracks) => {
+    try {
+        const tracks = await getQueue(db);
         db.close();
         res.send({ tracks });
-    });
+    } catch (err) {
+        res.status(500);
+        res.send({ error: err });
+    }
 });
 
-app.post('/api/queue', (req, res) => {
-    const db = getDb();
+app.post('/api/queue', async (req, res) => {
     const track: Track = req.body;
 
-    addTrackToQueue(db, track, (err) => {
+    try {
+        const db = getDb();
+        await addTrackToQueue(db, track);
         db.close();
-
-        if (err) {
-            console.log(`${new Date().toISOString()} - failed to add "${track.track_name}" to queue, `, err, '\n');
-
-            res.status(500);
-            return res.send({ error: err });
-        }
-
         console.log(`${new Date().toISOString()} - added "${track.track_name}" to queue \n`);
         res.send({ status: 'ok' });
-    });
+
+    } catch(err) {
+        console.log(`${new Date().toISOString()} - failed to add "${track.track_name}" to queue, `, err, '\n');
+        res.status(500);
+        return res.send({ error: err });
+    }
 });
 
-app.post('/api/vote', (req, res) => {
-    const db = getDb();
+app.post('/api/vote', async (req, res) => {
     const track: Track = req.body;
 
-    voteOnTrack(db, track, err => {
+    try {
+        const db = getDb();
+        await voteOnTrack(db, track);
         db.close();
-
-        if (err) {
-            console.log(`${new Date().toISOString()} - failed to vote for "${track.track_name}", `, err, '\n');
-
-            res.status(500);
-            return res.send({ error: err });
-        }
-
         console.log(`${new Date().toISOString()} - voted for "${track.track_name}" \n`);
         res.send({ status: 'ok' });
-    });
+    } catch(err) {
+        console.log(`${new Date().toISOString()} - failed to vote for "${track.track_name}", `, err, '\n');
+        res.status(500);
+        return res.send({ error: err });
+    }
 });
 
-app.get('/api/votes', (req, res) => {
+app.get('/api/votes', async (req, res) => {
     const db = getDb();
-    getAllVotes(db, (err, votes) => {
-        db.close();
 
-        if (err) {
-            res.status(500);
-            return res.send({ error: err });
-        }
+    try {
+        const votes = await getAllVotes(db);
         res.send({ votes });
-    });
+
+    } catch (err) {
+        res.status(500);
+        return res.send({ error: err });
+    }
 });
 
 let now: { cursor: number, track: null | Track } = {
@@ -81,42 +80,40 @@ let now: { cursor: number, track: null | Track } = {
     track: null,
 };
 
-function play() {
+async function play() {
     now.cursor = 0;
     now.track = null;
 
     const db = getDb();
-    const currentTrack = getQueue(db, (err, tracks) => {
-        if (!tracks || !tracks.length) {
-            setTimeout(play, 5000);
-            console.log(`${new Date().toISOString()} - no songs found, checking...`);
-            db.close();
+    const tracks = await getQueue(db);
+
+    if (!tracks || !tracks.length) {
+        setTimeout(play, 5000);
+        console.log(`${new Date().toISOString()} - no songs found, checking...`);
+        db.close();
+        return;
+    }
+
+    now.track = tracks[0];
+    await removeTrack(db, now.track.id);
+
+    console.log(`${new Date().toISOString()} - NOW PLAYING -> ${now.track.track_name}`);
+    db.close();
+    now.cursor = 0;
+
+    const intervalId = setInterval(() => {
+        if (!now.track) { return }
+        if (now.cursor < now.track.duration_ms) {
+            now.cursor += 100;
             return;
         }
 
-        now.track = tracks[0];
-
-        removeTrack(db, now.track.id, (err) => {
-            if (!now.track) { return }
-            console.log(`${new Date().toISOString()} - NOW PLAYING -> ${now.track.track_name}`);
-            db.close();
-            now.cursor = 0;
-
-            const intervalId = setInterval(() => {
-                if (!now.track) { return }
-                if (now.cursor < now.track.duration_ms) {
-                    now.cursor += 100;
-                    return;
-                }
-
-                console.log(`${new Date().toISOString()} - ENDED -> ${now.track.track_name}`);
-                clearInterval(intervalId);
-                play();
-            }, 100);
-        });
-
-    });
+        console.log(`${new Date().toISOString()} - ENDED -> ${now.track.track_name}`);
+        clearInterval(intervalId);
+        play();
+    }, 100);
 }
+
 app.get('/api/now', (req, res) => {
     if (!now.track) {
         return res.send({ status: 'no track is playing' });
--- server/db.ts
+++ server/db.ts
@@ -1,7 +1,28 @@
 import { Database } from 'sqlite3';
 
-type RowCallback<T> = (err: Error | null, rows: T) => void;
-type ErrorCallback = (err: Error | null) => void;
+function run(db: Database, sql: string): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        db.run(sql, (err) => {
+            err ? reject(err) : resolve();
+        });
+    });
+}
+
+function all<T>(db: Database, sql: string): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+        db.all(sql, (err, rows : T) => {
+            err ? reject(err) : resolve(rows);
+        });
+    });
+}
+
+function get<T>(db: Database, sql: string): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+        db.get(sql, (err, rows : T) => {
+            err ? reject(err) : resolve(rows);
+        });
+    });
+}
 
 export interface Track {
     id: number;
@@ -48,7 +69,7 @@ export function getDb(): Database {
 }
 
 // @TODO: protect against sql injections 
-export function getQueue(db: Database, callback: RowCallback<Track[]>) {
+export async function getQueue(db: Database) {
     const sql = `
         SELECT *, COUNT(votes.track_id) AS upvotes
         FROM queue, votes
@@ -56,48 +77,31 @@ export function getQueue(db: Database, callback: RowCallback<Track[]>) {
         GROUP BY queue.id
         ORDER BY upvotes DESC, last_updated ASC
     `;
-    db.all(sql, callback);
+    return all<Track[]>(db, sql);
 }
 
 // @TODO: protect against sql injections 
-export function addTrackToQueue(db: Database, track: Track, callback: ErrorCallback) {
-    db.run(`INSERT INTO queue (track_name, spotify_track_id, track_uri, duration_ms, user_id)
-            VALUES("${track.track_name}", "${track.spotify_track_id}", "${track.track_uri}", "${track.duration_ms}", "${track.user_id}")
-   `, (err) => {
-       if (err) {
-           return callback(err);
-       }
-
-       voteOnTrack(db, track, callback);
-   });
+export async function addTrackToQueue(db: Database, track: Track) {
+    await run(db, `INSERT INTO queue (track_name, spotify_track_id, track_uri, duration_ms, user_id)
+            VALUES("${track.track_name}", "${track.spotify_track_id}", "${track.track_uri}", "${track.duration_ms}", "${track.user_id}")`);
+    return voteOnTrack(db, track);
 }
 
-export function getTrackBySpotifyId(db: Database, spotifyTrackId: string, callback: RowCallback<Track>) {
-    db.get(`SELECT * FROM queue WHERE spotify_track_id='${spotifyTrackId}' LIMIT 1`, callback);
+export async function getTrackBySpotifyId(db: Database, spotifyTrackId: string) {
+    return get<Track>(db, `SELECT * FROM queue WHERE spotify_track_id='${spotifyTrackId}' LIMIT 1`);
 }
 
-export function voteOnTrack(db: Database, track: Track, callback: ErrorCallback) {
-    getTrackBySpotifyId(db, track.spotify_track_id, (err, track) => {
-        if (err) {
-            return callback(err);
-        }
-
-        db.run(`INSERT INTO votes (user_id, track_id)
-                VALUES("${track.user_id}", "${track.id}")
-        `, callback);
-    })
+export async function voteOnTrack(db: Database, track: Track) {
+    const trackFromDb = await getTrackBySpotifyId(db, track.spotify_track_id);
+    return run(db, `INSERT INTO votes (user_id, track_id) VALUES("${trackFromDb.user_id}", "${trackFromDb.id}")`);
 }
 
-export function removeTrack(db: Database, trackId: number, callback: ErrorCallback) {
-    db.run(`DELETE FROM queue WHERE id='${trackId}'`, err => {
-        if (err) {
-            return callback(err);
-        }
-        db.run(`DELETE FROM votes WHERE track_id='${trackId}'`, callback);
-    });
+export async function removeTrack(db: Database, trackId: number) {
+    await run(db, `DELETE FROM queue WHERE id='${trackId}'`);
+    await run(db, `DELETE FROM votes WHERE track_id='${trackId}'`);
 }
 
-export function getAllVotes(db: Database, callback: RowCallback<Vote>) {
-    db.all(`SELECT * FROM votes ORDER BY track_id`, callback);
+export async function getAllVotes(db: Database) {
+    return all<Vote>(db, `SELECT * FROM votes ORDER BY track_id`);
 }
 
