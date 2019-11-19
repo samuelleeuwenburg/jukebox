916b29d136cf06fe9ce6ab811928739e87d151c9
Samuel Leeuwenburg <samleeuwenburg@gmail.com>
2019-11-20 00:25:10 +0100

    rework backend to sockets

--- server/app.ts
+++ server/app.ts
@@ -1,104 +1,150 @@
-import express = require('express');
-import bodyParser = require('body-parser');
-import cors = require('cors');
-import { play, now } from './jukebox';
-import {
-    createDb,
-    getDb,
-    getQueue,
-    addTrackToQueue,
-    Track,
-    voteOnTrack,
-    removeTrack,
-    getAllUserVotes,
-    emitQueueUpdate
-} from './db';
+import express = require('express')
+import cors = require('cors')
+import path = require('path')
+import { Socket, Server } from 'socket.io'
 
-const app = express();
-const server = require('http').createServer(app);
-export const io = require('socket.io')(server, { path: '/socket.io' });
-createDb();
+const app = express()
+const server = require('http').createServer(app)
+const io: Server = require('socket.io')(server, { path: '/socket.io' })
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors())
 
-app.get('/api/queue', async (req, res) => {
-    const db = getDb();
+interface Track {
+    id: string;
+    name: string;
+    uri: string;
+    userId: string;
+    imageUrl: string;
+    durationMs: number;
+    upvotes: string[];
+    timestamp: number;
+}
 
-    try {
-        const tracks = await getQueue(db);
-        db.close();
-        res.send({ tracks });
-    } catch (err) {
-        res.status(500);
-        res.send({ error: err });
-    }
-});
+interface Vote {
+    userId: string;
+    timestamp: number;
+}
 
-app.post('/api/queue', async (req, res) => {
-    const track: Track = req.body;
+interface CurrentTrack {
+    track: Track;
+    position: number;
+    timestamp: number;
+}
 
-    try {
-        const db = getDb();
-        await addTrackToQueue(db, track);
+interface Queue {
+    tracks: Track[];
+    currentTrack: CurrentTrack | null;
+}
 
-        await emitQueueUpdate(db);
-
-        db.close();
-        console.log(`${new Date().toISOString()} - added "${track.track_name}" to queue \n`);
-
-
-        res.send({ status: 'ok' });
-
-    } catch(err) {
-        console.log(`${new Date().toISOString()} - failed to add "${track.track_name}" to queue, `, err, '\n');
-        res.status(500);
-        return res.send({ error: err });
-    }
-});
-
-app.post('/api/vote', async (req, res) => {
-    const track: Track = req.body;
-
-    try {
-        const db = getDb();
-        await voteOnTrack(db, track);
-        await emitQueueUpdate(db);
-        
-        db.close();
-        console.log(`${new Date().toISOString()} - voted for "${track.track_name}" \n`);
-        res.send({ status: 'ok' });
-    } catch(err) {
-        console.log(`${new Date().toISOString()} - failed to vote for "${track.track_name}", `, err, '\n');
-        res.status(500);
-        return res.send({ error: err });
-    }
-});
-
-app.get('/api/votes/:userId', async (req, res) => {
-    const db = getDb();
-
-    try {
-        const userId = req.params.userId;
-
-        const votes = await getAllUserVotes(db, userId);
-        res.send({ votes });
-
-    } catch (err) {
-        res.status(500);
-        return res.send({ error: err });
-    }
-});
-
-app.get('/api/now', (req, res) => {
-    if (!now.track) {
-        return res.send({ status: 'no track is playing' });
-    }
-
-    res.send(now);
-});
+app.get('/debug', (req, res) => {
+    res.sendFile(path.join(__dirname + '/debug.html'))
+})
 
 server.listen(3000, () => {
-    play();
-    console.log('Jukebox is playing on port 3000!');
-});
+    console.log('Jukebox is playing on port 3000!')
+    const state = getInitialState();
+
+    loop(state, io)
+
+    io.on('connect', (socket: Socket) => {
+        socket.on('vote', (data: { trackId: string, userId: string }) => {
+            log('received vote for ->', data.trackId)
+            voteOnTrack(state, data.trackId, data.userId)
+            io.emit('newQueue', state)
+        })
+
+        socket.on('addTrack', (partialTrack: Track) => {
+            log('received track', partialTrack)
+            const track: Track = {
+                ...partialTrack,
+                timestamp: Date.now(),
+                upvotes: [partialTrack.userId]
+            }
+
+            addTrack(state, track)
+        })
+
+        socket.on('getQueue', () => {
+            socket.emit('newQueue', state)
+        })
+    })
+})
+
+function loop(state: Queue, io: Server) {
+    state.currentTrack = getNewCurrentTrack(state)
+
+    if (!state.currentTrack) {
+        setTimeout(() => loop(state, io), 2000)
+        log('no tracks in queue, waiting...')
+        return
+    }
+
+    removeTrack(state, state.currentTrack.track.id)
+    log('NOW PLAYING ->', state.currentTrack.track)
+    io.emit('newQueue', state)
+
+    const intervalId = setInterval(() => {
+        if (!state.currentTrack) {
+            clearInterval(intervalId)
+            loop(state, io)
+            return
+        }
+
+        const songEndsAt = state.currentTrack.timestamp
+            + state.currentTrack.track.durationMs
+        const now = Date.now()
+        if (now > songEndsAt) {
+            log('ENDED ->', state.currentTrack.track)
+            clearInterval(intervalId)
+            loop(state, io)
+        }
+    }, 100)
+}
+
+function getInitialState(): Queue {
+    return { tracks: [], currentTrack: null }
+}
+
+function getNewCurrentTrack(state: Queue): CurrentTrack | null {
+    if (!state.tracks.length) {
+        return null
+    }
+
+    sortQueue(state)
+    const track = state.tracks[0]
+
+    return {
+        track,
+        timestamp: Date.now(),
+        position: 0,
+    }
+}
+
+function voteOnTrack(state: Queue, trackId: string, userId: string) {
+    state.tracks = state.tracks.map(track => {
+        return track.id === trackId && !track.upvotes.includes(userId)
+            ? {...track, upvotes: [...track.upvotes, userId] }
+            : track
+    })
+    sortQueue(state)
+}
+
+function addTrack(state: Queue, track: Track) {
+    state.tracks = [...state.tracks, track]
+    sortQueue(state)
+}
+
+function removeTrack(state: Queue, trackId: string) {
+    log('removing track ->', trackId)
+    state.tracks = state.tracks.filter(track => track.id !== trackId)
+}
+
+function sortQueue(state: Queue) {
+    state.tracks = state.tracks
+        .sort((a, b) => a.upvotes.length > b.upvotes.length ? 1 : -1)
+}
+
+function log(...messages: any[]) {
+    console.log(new Date().toISOString(), ...messages)
+}
+
